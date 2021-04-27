5020332bf9ff6d2389a520b3ebf33b33b7555d52
dooly7183 <dooly7183@naver.com>
2021-04-27 14:20:00 +0900

    feat: 🍎 [BE] 필요한 파라미터 받아오는 routeController,ServiceImpl구현
    [BE] 필요한 파라미터 받아오는 routeController,ServiceImpl구현
    
    ✅ Closes: S04P31A104-128

--- backend/FinalPJT_A104/src/main/java/com/ssafy/project/model/route/RouteFindRequest.java
+++ backend/FinalPJT_A104/src/main/java/com/ssafy/project/model/route/RouteFindRequest.java
@@ -6,11 +6,11 @@ import lombok.Data;
 public class RouteFindRequest {
 	
 	//출발지 x,y
-		private float SX;
-		private float SY;
-		//도착지 x,y
-		private float EX;
-		private float EY;
-		//도착시간
-		private String arriveTime;
+	private float SX;
+	private float SY;
+	//도착지 x,y
+	private float EX;
+	private float EY;
+	//도착시간
+	private String arriveTime;
 }
--- backend/FinalPJT_A104/src/main/java/com/ssafy/project/service/route/RouteServiceImpl.java
+++ backend/FinalPJT_A104/src/main/java/com/ssafy/project/service/route/RouteServiceImpl.java
@@ -8,20 +8,42 @@ import javax.net.ssl.HttpsURLConnection;
 
 import org.springframework.stereotype.Service;
 
+import com.ssafy.project.model.route.RouteFindRequest;
+
 @Service
 public class RouteServiceImpl implements RouteService {
 
-	/* API URL 주소 - 버스 호선으로 검색 */
-//	private final String openUrl = "https://api.odsay.com/v1/api/searchBusLane?busNo=10&CID=1000&apiKey=6f0ylgh5xFio64cNOgk4FUwGA0LhfyiH0hi5eLERJBA";
+	@Override
+	public String findRoute(RouteFindRequest routeFindRequest) {
+		//대중교통 길찾기 api(실행시킬때 본인 api 키 값으로 바꾸기)
+		final String openUrl = "https://api.odsay.com/v1/api/searchPubTransPathT?lang=0&SX="+routeFindRequest.getSX()+"&SY="+routeFindRequest.getSY()+"&EX="+routeFindRequest.getEX()+"&EY="+routeFindRequest.getEY()+"&apiKey=D6BmCrs4iH/PLaOQ390EUYI9%2BAdf8B55184hmV7GpSA";
+		
+		StringBuffer sb = new StringBuffer();
+		
+		try {
+			URL url = new URL(openUrl);
 
-	/* API URL 주소 - 경로로 검색 */
-	private final String openUrl = "https://api.odsay.com/v1/api/searchBusLane?busNo=10&CID=1000&apiKey=D6BmCrs4iH/PLaOQ390EUYI9%2BAdf8B55184hmV7GpSA";
+			HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+			urlConnection.setRequestMethod("GET");
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+
+			sb.append(br.readLine());
+
+			urlConnection.disconnect();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return sb.toString();	
+	}
 	
 	@Override
 	public String readApi() {
 		
 		StringBuffer sb = new StringBuffer();
-
+		final String openUrl = "https://api.odsay.com/v1/api/searchBusLane?busNo=10&CID=1000&apiKey=D6BmCrs4iH/PLaOQ390EUYI9%2BAdf8B55184hmV7GpSA";
+		
 		try {
 			URL url = new URL(openUrl);
 
