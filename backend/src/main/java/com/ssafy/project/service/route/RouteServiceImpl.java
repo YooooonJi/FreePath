c7c58f22b6f3a14f8ff73f9e5b41b7be3e3fafe1
dooly7183 <dooly7183@naver.com>
2021-04-29 17:26:45 +0900

    feat: 🍎 [BE] ROUTE 실시간 버스 반영
    [BE] ROUTE 실시간 버스 반영
    
    ✅ Closes: S04P31A104-136

--- backend/src/main/java/com/ssafy/project/service/route/RouteServiceImpl.java
+++ backend/src/main/java/com/ssafy/project/service/route/RouteServiceImpl.java
@@ -3,12 +3,17 @@ package com.ssafy.project.service.route;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.StringTokenizer;
 
 import javax.net.ssl.HttpsURLConnection;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.project.model.route.RouteFindRequest;
+
 
 @Service
 public class RouteServiceImpl implements RouteService {
@@ -21,15 +26,44 @@ public class RouteServiceImpl implements RouteService {
 		StringBuffer sb = new StringBuffer();
 		
 		try {
+			
 			URL url = new URL(openUrl);
-
 			HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
 			urlConnection.setRequestMethod("GET");
 
 			BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
-
-			sb.append(br.readLine());
-
+			
+			JSONParser parser=new JSONParser();
+			JSONObject obj=(JSONObject)parser.parse(br);
+			JSONObject response=(JSONObject)obj.get("result");
+			JSONArray path = (JSONArray) response.get("path");
+			
+			//세부 경로들
+			for (int i = 0; i < path.size()-1; i++) {
+				JSONObject infos=(JSONObject)path.get(i);
+				
+				//pathType : 1=지하철, 2=버스, 3=지하철+버스
+				long pathType=(Long)infos.get("pathType");
+				System.out.print(pathType+" ");
+				
+				JSONObject info=(JSONObject)infos.get("info");
+				
+				long totalTimeTmp=(long)info.get("totalTime");
+				int totalTime=(int)totalTimeTmp;
+				
+				//출발시간 구하기
+				String[] time=routeFindRequest.getArriveTime().split(":");
+				int hour=Integer.parseInt(time[0]);
+				int minute=Integer.parseInt(time[1]);
+				String startTime=(hour*60+minute-totalTime)/60+":"+(hour*60+minute-totalTime)%60;
+				
+				System.out.println("start:"+startTime);
+				String first=(String)info.get("firstStartStation");
+				System.out.print(first +" "+totalTime);
+				System.out.println();
+				
+			}
+			
 			urlConnection.disconnect();
 
 		} catch (Exception e) {
