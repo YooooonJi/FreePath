3395e7c745806233c51571a931cb12b463ec4026
wjd1rb <wjd1rb@gmail.com>
2021-05-12 14:26:07 +0900

    feat: 🍎 [BE] - 막차 시간 계산 및 초 단위 시간 전달
    - 막차 시간 계산 및 초 단위 시간 전달
    
    ✅ Closes: S04P31A104-205

--- backend/src/main/java/com/ssafy/project/service/route/FinalRouteServiceImpl.java
+++ backend/src/main/java/com/ssafy/project/service/route/FinalRouteServiceImpl.java
@@ -3,18 +3,21 @@ package com.ssafy.project.service.route;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
-
 import javax.net.ssl.HttpsURLConnection;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,20 +32,8 @@ public class FinalRouteServiceImpl implements FinalRouteService {
 	@Autowired
 	private ApiProperties apiProperties;
 
-	/*
-	 * 막차 계산하기 위한 루틴 
-	 * 1. 각 경로의 전체 경로 확인 O
-	 * 2. 마지막 경로부터 확인하는데 마지막 도로 경로일 경우 제외 O
-	 * 3. 경로마다 출발지에서 도착지까지 가는 막차 시간 구하기 O
-	 * 4. 각 경로의 가장 늦게 출발할 수 있는 시간 저장 O
-	 * 4-1. 지하철 막차 시간 구하기 O 
-	 * 4-2. 버스 막차 시간 구하기 O
-	 * 4-3. 도로 시간은 현재 시간에서 빼기 O 
-	 * 5. 가장 늦게 출발할 수 있는 순으로 정렬 => 가장 늦게 탈 수 있는 경로 전달(마지막 차가 동일한 경우에는 더 짧은 경우를 전달) O 
-	 * sectionTime => 각 구간 이동 소요 시간 
-	 * trafficType => 1-지하철, 2-버스, 3-도보 
-	 * wayCode => 1: 상행, 2: 하행
-	 */
+	public static final Logger logger = LoggerFactory.getLogger(FinalRouteServiceImpl.class);
+
 	@Override
 	public ResponseEntity<Map<String, Object>> findLast(RouteFindRequest routeFindRequest) {
 
@@ -65,23 +56,46 @@ public class FinalRouteServiceImpl implements FinalRouteService {
 			JSONObject response = (JSONObject) obj.get("result");
 			JSONArray path = (JSONArray) response.get("path");
 
-			Calendar cal = Calendar.getInstance(); // 오늘 날짜 => 요일을 확인하기 위해서
-			int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK); // 1: 일요일 ~ 7: 토요일
-
 			int lastOneIdx = 0; // 가장 늦게 탈 수 있는 경로의 번호
 			int lastOneTime = Integer.MIN_VALUE; // 가장 늦게 탈 수 있는 경로의 출발 시간 (1245 => 12시 45분 출발)
+			long remainSecond = 0; // 도착시간까지 남은 시간
+			int[] dayOfMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+			/* 사용자가 원하는 도착시간 정보 */
+			String[] arriveTime = routeFindRequest.getArriveTime().split(" ");
+			int arriveMonth = Integer.parseInt(arriveTime[0].substring(5, 7)); // 월
+			int arriveDay = Integer.parseInt(arriveTime[0].substring(8, 10)); // 일
+
+			Calendar today = Calendar.getInstance(); // 오늘 날짜 => 요일을 확인하기 위해서
+			today.setTime(new Date());
+
+			int dayOfWeek = today.get(Calendar.DAY_OF_WEEK); // 1: 일요일 ~ 7: 토요일
+			int nowMonth = today.get(Calendar.MONTH) + 1;
+			int nowDay = today.get(Calendar.DATE);
+
+			if (arriveMonth != nowMonth) { // 월이 다른 경우
+				dayOfWeek += ((dayOfMonth[nowMonth] - nowDay) + arriveDay);
+
+				if (dayOfWeek > 7)
+					dayOfWeek -= 7;
+			} else {
+				if (arriveDay != nowDay) { // 일이 다른 경우, 도착 일자에 맞추기 위해서
+					dayOfWeek += (arriveDay - nowDay);
+
+					if (dayOfWeek > 7)
+						dayOfWeek -= 7;
+				}
+			}
 
 			for (int i = 0; i < path.size(); i++) {
-//				System.out.println("===============" + i + "=================");
+				// System.out.println("===============" + i + "=================");
 				JSONObject paths = (JSONObject) path.get(i);
 
 				/* pathType => 1: 지하철, 2: 버스, 3: 지하철&버스 --> 이후 개인화 과정에서 필요 */
-				int pathType = Integer.parseInt(String.valueOf(paths.get("pathType")));
-//				System.out.println("pathType: " + pathType);
+//				int pathType = Integer.parseInt(String.valueOf(paths.get("pathType")));
 
 				/* 경로의 전반적인 정보 */
-				JSONObject info = (JSONObject) paths.get("info");
-//				System.out.println("info: " + info);
+//				JSONObject info = (JSONObject) paths.get("info");
 
 				/* 각 전체 경로 확인 */
 				JSONArray subPath = (JSONArray) paths.get("subPath");
@@ -89,34 +103,29 @@ public class FinalRouteServiceImpl implements FinalRouteService {
 
 				for (int j = subPath.size() - 1; j >= 0; j--) {
 					JSONObject smallSubPath = (JSONObject) subPath.get(j);
-//					System.out.println("smallSubPath : " + smallSubPath);
 					int trafficType = Integer.parseInt(String.valueOf(smallSubPath.get("trafficType")));
 
 					/* 모든 TrafficType에 대해서 구간 시간을 뺀 뒤 그 시간에 탈 수 있는 막차를 구해야함! */
 					subPathStartTime -= Integer.parseInt(String.valueOf(smallSubPath.get("sectionTime")));
 					if (subPathStartTime % 100 >= 60)
 						subPathStartTime -= 40;
-					
+
 					if (trafficType == 1) { // 지하철
-//						System.out.println("-------------지하철-------------");
 						int subwayStartId = Integer.parseInt(String.valueOf(smallSubPath.get("startID"))); // 출발역의 ID
 						int subwayCode = Integer.parseInt(String.valueOf(smallSubPath.get("wayCode"))); // 상, 하행 정보
 
 						/* 지하철 막차의 출발 시간 반환 */
 						subPathStartTime = findSubwayLastStartTime(subwayStartId, subwayCode, subPathStartTime, dayOfWeek);
 					} else if (trafficType == 2) { // 버스
-//						System.out.println("-------------버스-------------");
-						int busStationStartId = Integer.parseInt(String.valueOf(smallSubPath.get("startID"))); // 출발 정류장 ID
-
 						JSONArray laneInfos = (JSONArray) smallSubPath.get("lane");
 						JSONObject laneInfo = (JSONObject) laneInfos.get(0);
 						int busId = Integer.parseInt(String.valueOf(laneInfo.get("busID")));
+						int busStationStartId = Integer.parseInt(String.valueOf(smallSubPath.get("startID"))); // 출발 정류장 ID
 
+						/* 버스 막차의 출발 시간 반환 */
 						subPathStartTime = findBusLastStartTime(busStationStartId, busId, subPathStartTime, dayOfWeek);
 					}
 				}
-				
-//				System.out.println("subPathStartTime : " + subPathStartTime);
 
 				/* 가장 늦게 탈 수 있는 경로 확인 */
 				if (lastOneTime < subPathStartTime) {
@@ -125,98 +134,46 @@ public class FinalRouteServiceImpl implements FinalRouteService {
 				}
 			}
 
-			if (lastOneTime >= 2400)
+			/* 막차 도착 시간 만들기 */
+			StringBuilder sb = new StringBuilder();
+			sb.append(arriveTime[0].substring(0, 4) + "-"); // 년
+
+			if ((dayOfMonth[arriveMonth] == arriveDay) && lastOneTime >= 2400) { // 월이 넘어가는 경우
+				arriveMonth++;
+				arriveDay = 1;
+			} else if (lastOneTime >= 2400) {
+				arriveDay++;
 				lastOneTime -= 2400;
-			
-//			System.out.println("lastOneTime : " + lastOneTime);
-			resultMap.put("lastTime", lastOneTime); // 전달 방식을 바꿀 여지가 있음
+			}
+
+			sb.append(arriveMonth + "-"); // 월
+			sb.append(arriveDay + " "); // 일
+			sb.append((lastOneTime / 100) + ":" + (lastOneTime % 100));
+
+			/* 현재 시간으로부터 막차 도착시간까지 남은 시간 초로 전달 */
+			String[] lastTime = sb.toString().split(" ");
+
+			Date date = new SimpleDateFormat("yyyy-MM-dd").parse(lastTime[0]);
+			Calendar lastDate = Calendar.getInstance();
+			lastDate.setTime(date);
+
+			remainSecond = (lastDate.getTimeInMillis() - today.getTimeInMillis()) / 1000;
+
+			resultMap.put("lastTime", sb.toString());
 			resultMap.put("lastOne", (JSONObject) path.get(lastOneIdx));
+			resultMap.put("remainSecond", remainSecond);
 
 			urlConnection.disconnect();
 			status = HttpStatus.OK;
 		} catch (Exception e) {
-			e.printStackTrace();
+			logger.error("막차 시간 계산 실패 : {}", e);
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
 		}
 
 		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
 
-	public int findBusLastStartTime(int busStationStartId, int busId, int startTime, int dayOfWeek) {
-
-//		System.out.println("startTime : " + startTime);
-
-		StringTokenizer st = null;
-		int newTime = 0;
-
-		final String busRouteInfoOpenUrl = "https://api.odsay.com/v1/api/busLaneDetail?lang=0&busID=" + busId + "&apiKey=" + apiProperties.getKey();
-
-		try {
-			URL busUrl = new URL(busRouteInfoOpenUrl);
-
-			HttpsURLConnection busUrlConnection = (HttpsURLConnection) busUrl.openConnection();
-			busUrlConnection.setRequestMethod("GET");
-
-			BufferedReader br = new BufferedReader(new InputStreamReader(busUrlConnection.getInputStream(), "UTF-8"));
-
-			JSONParser parser = new JSONParser();
-			JSONObject obj = (JSONObject) parser.parse(br);
-			JSONObject response = (JSONObject) obj.get("result");
-
-			/* 해당 노선의 막차 시간 가져오기 */
-			String busLastTime = (String) response.get("busLastTime");
-//			System.out.println("busLastTime : " + busLastTime);
-
-			st = new StringTokenizer(busLastTime, ":");
-			newTime += Integer.parseInt(st.nextToken()) * 100; // 시
-			newTime += Integer.parseInt(st.nextToken()); // 분
-
-			/* 요일에 따른 버스 간격 결정 */
-			int busIntevalWeek = 0;
-			if (dayOfWeek == 1) { // 일요일
-				busIntevalWeek = Integer.parseInt(String.valueOf(response.get("bus_Interval_Sun")));
-			} else if (dayOfWeek == 7) { // 토요일
-				busIntevalWeek = Integer.parseInt(String.valueOf(response.get("bus_Interval_Sat")));
-			} else { // 평일
-				busIntevalWeek = Integer.parseInt(String.valueOf(response.get("bus_Interval_Week")));
-			}
-
-			/* 버스 출발 정거장의 위치를 확인 후 막차 시간 계산 */
-			JSONArray stationList = (JSONArray) response.get("station");
-			for (int i = 0; i < stationList.size(); i++) {
-				JSONObject station = (JSONObject) stationList.get(i);
-
-				if (Integer.parseInt(String.valueOf(station.get("stationID"))) == busStationStartId) {
-					int stationDistance = Integer.parseInt(String.valueOf(station.get("stationDistance")));
-
-					/* 서울 버스의 평균 속도를 20km/h라는 가정을 하고 막차 시간 계산 */
-					newTime += (int) Math.ceil((stationDistance * 60) / 20000);
-				}
-			}
-			
-			// 분이 60을 넘어가는 경우
-			if (newTime % 100 >= 60)
-				newTime += 40;
-			
-			/* 확인된 막차 시간에서 interval을 빼면서 startTime보다 작은 가장 늦은 시간 전달 */
-			while (newTime > startTime) {
-				newTime -= busIntevalWeek;
-				
-				if (newTime % 100 >= 60)
-					newTime -= 40;
-			}
-			
-			busUrlConnection.disconnect();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-//		System.out.println("newTime : " + newTime);
-		return newTime;
-	}
-
 	public int findSubwayLastStartTime(int subwayStartId, int subwayCode, int startTime, int dayOfWeek) {
-
-//		System.out.println("startTime : " + startTime);
 
 		StringTokenizer st = null;
 		int newTime = 0;
@@ -253,7 +210,6 @@ public class FinalRouteServiceImpl implements FinalRouteService {
 			} else if (subwayCode == 2) { // 하행
 				wayList = (JSONObject) subwayList.get("down");
 			}
-			// System.out.println(wayList.toString());
 
 			/* 가장 마지막 시간의 지하철 시간 받아오기 */
 			JSONArray timeList = (JSONArray) wayList.get("time");
@@ -261,7 +217,6 @@ public class FinalRouteServiceImpl implements FinalRouteService {
 				JSONObject lastTimeList = (JSONObject) timeList.get(i);
 
 				int hour = Integer.parseInt(String.valueOf(lastTimeList.get("Idx"))); // 시간
-//				 System.out.println("hour : " + hour);
 
 				if (hour <= (startTime / 100)) { // 우선 동일한 시간인 경우를 확인
 					if ((startTime / 100) == hour && (startTime % 100) == 0) // 시간이 같은데 0분이 경우는 무조건 전 시간 가장 늦은 애로!
@@ -272,7 +227,6 @@ public class FinalRouteServiceImpl implements FinalRouteService {
 
 					// 2. 분 정하기
 					String minute = (String) lastTimeList.get("list");
-					// System.out.println("minute : " + minute);
 					st = new StringTokenizer(minute, " ");
 
 					ArrayList<Integer> minuteList = new ArrayList<Integer>();
@@ -292,16 +246,83 @@ public class FinalRouteServiceImpl implements FinalRouteService {
 							}
 						}
 					}
-
 				}
 			}
 
 			subwayUrlConnection.disconnect();
 		} catch (Exception e) {
-			e.printStackTrace();
+			logger.error("지하철 막차 시간 계산 실패 : {}", e);
 		}
 
-//		System.out.println("newTime : " + newTime);
+		return newTime;
+	}
+
+	public int findBusLastStartTime(int busStationStartId, int busId, int startTime, int dayOfWeek) {
+
+		StringTokenizer st = null;
+		int newTime = 0;
+
+		final String busRouteInfoOpenUrl = "https://api.odsay.com/v1/api/busLaneDetail?lang=0&busID=" + busId + "&apiKey=" + apiProperties.getKey();
+
+		try {
+			URL busUrl = new URL(busRouteInfoOpenUrl);
+
+			HttpsURLConnection busUrlConnection = (HttpsURLConnection) busUrl.openConnection();
+			busUrlConnection.setRequestMethod("GET");
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(busUrlConnection.getInputStream(), "UTF-8"));
+
+			JSONParser parser = new JSONParser();
+			JSONObject obj = (JSONObject) parser.parse(br);
+			JSONObject response = (JSONObject) obj.get("result");
+
+			/* 해당 노선의 막차 시간 가져오기 */
+			String busLastTime = (String) response.get("busLastTime");
+
+			st = new StringTokenizer(busLastTime, ":");
+			newTime += Integer.parseInt(st.nextToken()) * 100; // 시
+			newTime += Integer.parseInt(st.nextToken()); // 분
+
+			/* 요일에 따른 버스 간격 결정 */
+			int busIntevalWeek = 0;
+			if (dayOfWeek == 1) { // 일요일
+				busIntevalWeek = Integer.parseInt(String.valueOf(response.get("bus_Interval_Sun")));
+			} else if (dayOfWeek == 7) { // 토요일
+				busIntevalWeek = Integer.parseInt(String.valueOf(response.get("bus_Interval_Sat")));
+			} else { // 평일
+				busIntevalWeek = Integer.parseInt(String.valueOf(response.get("bus_Interval_Week")));
+			}
+
+			/* 버스 출발 정거장의 위치를 확인 후 막차 시간 계산 */
+			JSONArray stationList = (JSONArray) response.get("station");
+			for (int i = 0; i < stationList.size(); i++) {
+				JSONObject station = (JSONObject) stationList.get(i);
+
+				if (Integer.parseInt(String.valueOf(station.get("stationID"))) == busStationStartId) {
+					int stationDistance = Integer.parseInt(String.valueOf(station.get("stationDistance")));
+
+					/* 서울 버스의 평균 속도를 20km/h라는 가정을 하고 막차 시간 계산 */
+					newTime += (int) Math.ceil((stationDistance * 60) / 20000);
+				}
+			}
+
+			// 분이 60을 넘어가는 경우
+			if (newTime % 100 >= 60)
+				newTime += 40;
+
+			/* 확인된 막차 시간에서 interval을 빼면서 startTime보다 작은 가장 늦은 시간 전달 */
+			while (newTime > startTime) {
+				newTime -= busIntevalWeek;
+
+				if (newTime % 100 >= 60)
+					newTime -= 40;
+			}
+
+			busUrlConnection.disconnect();
+		} catch (Exception e) {
+			logger.error("버스 막차 시간 계산 실패 : {}", e);
+		}
+
 		return newTime;
 	}
 }
