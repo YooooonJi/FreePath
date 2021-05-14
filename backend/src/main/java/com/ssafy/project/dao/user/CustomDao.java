f41a2b34d7f919c1f53840f78143056cf2bb147f
wjd1rb <wjd1rb@gmail.com>
2021-05-14 15:43:28 +0900

    feat: 🍎 [DB] - Spring 연동 및 test
    - Spring 연동 및 test
    
    ✅ Closes: S04P31A104-222

---
+++ backend/src/main/java/com/ssafy/project/dao/user/CustomDao.java
@@ -0,0 +1,10 @@
+package com.ssafy.project.dao.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.ssafy.project.model.user.Custom;
+
+@Repository
+public interface CustomDao extends JpaRepository<Custom, String>{
+
+}
