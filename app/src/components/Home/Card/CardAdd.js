c7a708661150fabf6c9c21da3a2f1c01bbb371e4
Nanjae <dngngn3045@gmail.com>
2021-05-16 21:40:55 +0900

    feat: 🍎 [FE] 시간 설정 추가 및 결과 값 확인 구현
    - 시간 설정 추가 및 결과 값 확인 구현
    
    ✅ Closes: #S04P31A104-197

--- app/src/components/Home/Card/CardAdd.js
+++ app/src/components/Home/Card/CardAdd.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components/native";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import DateTimePicker from "@react-native-community/datetimepicker";
 import TypeBus from "./TypeBus";
 import TypeSubway from "./TypeSubway";
 import TypePath from "./TypePath";
@@ -61,7 +62,7 @@ const SelectBox = styled.TouchableOpacity`
   padding: 5px 10px 5px 10px;
   height: 25px;
   border-radius: 8px;
-  background-color: #ce5a5a;
+  background-color: ${(props) => (props.focus ? "#ce5a5a" : "#d0d0d0")};
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -119,10 +120,18 @@ const ButtonText = styled.Text`
 `;
 
 const CardAdd = ({ setPopCardAdd }) => {
-  const [alarmName, setAlarmName] = useState(null);
-  const [alarmType, setAlarmType] = useState("경로");
+  const [alarmName, setAlarmName] = useState("");
+  // 알람종류 => 경로 : 0, 버스 : 1, 지하철 : 2
+  const [alarmType, setAlarmType] = useState(0);
   const [inputValue1, setInputValue1] = useState(null);
   const [inputValue2, setInputValue2] = useState(null);
+  // 시간종류 => 막차 : 0, 시간선택 : 1
+  const [timeType, setTimeType] = useState(0);
+  const [alarmTime, setAlarmTime] = useState("");
+
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
 
   const getInputValue1 = (input1) => {
     setInputValue1(input1);
@@ -134,30 +143,79 @@ const CardAdd = ({ setPopCardAdd }) => {
 
   let inputType;
 
-  if (alarmType === "경로") {
+  if (alarmType === 0) {
     inputType = (
       <TypePath
         getInputValue1={getInputValue1}
         getInputValue2={getInputValue2}
       />
     );
-  } else if (alarmType === "버스") {
+  } else if (alarmType === 1) {
     inputType = <TypeBus />;
   } else {
     inputType = <TypeSubway />;
   }
 
+  const onPressAlarmTime = () => {
+    setTimeType(1);
+    setShow(true);
+  };
+
+  // api 호출 일어날 곳
   const saveCard = () => {
-    if (alarmName === null || inputValue1 === null || inputValue2 === null) {
-      alert("알람 정보를 입력해주세요.");
+    if (alarmName === "") {
+      alert("알림 이름을 입력해주세요.");
+    } else if (inputValue1 === null) {
+      alert("출발지를 입력해주세요");
+    } else if (inputValue2 === null) {
+      alert("도착지를 입력해주세요");
+    } else if (timeType === 1 && alarmTime === "") {
+      alert("시간을 설정해주세요");
     } else {
-      alert(`저장 - ${alarmType} : ${inputValue1} / ${inputValue2}`);
+      alert(
+        `알림이름 : ${alarmName}\n알림타입 : ${alarmType}\n${inputValue1.address_name}\n[${inputValue1.x}, ${inputValue1.y}]\n${inputValue2.address_name}\n[${inputValue2.x}, ${inputValue2.y}]\n알림타입 : ${timeType}\n설정시간 : ${alarmTime} `
+      );
     }
   };
 
   return (
     <CardAddContainer>
-      <CardAddBgOpacity onPress={() => setPopCardAdd(false)} />
+      <CardAddBgOpacity onPress={() => setPopCardAdd(false)}>
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode={mode}
+            is24Hour
+            display="spinner"
+            onChange={(event, selected) => {
+              if (event.type === "dismissed") {
+                setShow(false);
+                return;
+              }
+
+              if (selected !== undefined) {
+                if (mode === "date") {
+                  setDate(selected);
+                  setMode("time");
+                }
+
+                if (mode === "time") {
+                  setAlarmTime(
+                    `${JSON.stringify(selected.toJSON()).slice(
+                      1,
+                      11
+                    )} ${JSON.stringify(selected.toJSON()).slice(12, 17)}`
+                  );
+
+                  setShow(false);
+                  setMode("date");
+                }
+              }
+            }}
+          />
+        )}
+      </CardAddBgOpacity>
       <CardAddInner>
         <TitleText>알림 추가</TitleText>
         <SubTitleContainer>
@@ -174,32 +232,32 @@ const CardAdd = ({ setPopCardAdd }) => {
           <SubTitleText>종류 선택</SubTitleText>
         </SubTitleContainer>
         <SelectContainer>
-          <SelectBox onPress={() => setAlarmType("경로")}>
+          <SelectBox focus={alarmType === 0} onPress={() => setAlarmType(0)}>
             <Icon name="map" size={15} color="#FFFFFF" />
             <SelectText>경로</SelectText>
           </SelectBox>
-          <SelectBox onPress={() => setAlarmType("버스")}>
+          <SelectBox focus={alarmType === 1} onPress={() => setAlarmType(1)}>
             <Icon name="directions-bus" size={15} color="#FFFFFF" />
             <SelectText>버스</SelectText>
           </SelectBox>
-          <SelectBox onPress={() => setAlarmType("지하철")}>
+          <SelectBox focus={alarmType === 2} onPress={() => setAlarmType(2)}>
             <Icon name="tram" size={15} color="#FFFFFF" />
             <SelectText>지하철</SelectText>
           </SelectBox>
         </SelectContainer>
         {inputType}
         <SubTitleContainer>
-          <SubTitleText>출발 시간 설정</SubTitleText>
+          <SubTitleText>도착 시간 설정</SubTitleText>
           <Icon name="info" size={15} color="#000000" />
         </SubTitleContainer>
         <SelectContainer>
-          <SelectBox>
+          <SelectBox focus={timeType === 0} onPress={() => setTimeType(0)}>
             <Icon name="bedtime" size={15} color="#FFFFFF" />
             <SelectText>막차</SelectText>
           </SelectBox>
-          <SelectBox>
+          <SelectBox focus={timeType === 1} onPress={onPressAlarmTime}>
             <Icon name="access-time" size={15} color="#FFFFFF" />
-            <SelectText>시간 선택</SelectText>
+            <SelectText>{alarmTime === "" ? "시간선택" : alarmTime}</SelectText>
           </SelectBox>
         </SelectContainer>
         <ButtonContainer>
